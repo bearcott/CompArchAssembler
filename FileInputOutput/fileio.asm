7a2ef03a1b37da11f33baa1bdeca99f9a2bb1b59
Sai Gatram <gatramsai@gmail.com>
2016-04-23 13:39:56 -0500

    Added a load byte "loop"
    You can see with the increments of 0,1, and 2, "add is printed out." We
    just need to loop that for the terms or until a space, /n, or ",", to
    loop properly.

--- FileInputOutput/fileio.asm
+++ FileInputOutput/fileio.asm
@@ -1,30 +1,53 @@
 .data  
 fin: .asciiz "test.asm"      # filename for input
 buffer: .space 1024
+myString: .word 268501009
+register: .space 4
 
 .text
 #open a file for writing
-li   $v0, 13       # system call for open file
-la   $a0, fin      # board file name
-li   $a1, 0        # Open for reading
-li   $a2, 0
-syscall            # open a file (file descriptor returned in $v0)
-move $s6, $v0      # save the file descriptor 
+	li   $v0, 13       # system call for open file
+	la   $a0, fin      # board file name
+	li   $a1, 0        # Open for reading
+	li   $a2, 0
+	syscall            # open a file (file descriptor returned in $v0)
+	move $s6, $v0      # save the file descriptor 
 
 #read from file
-li   $v0, 14       # system call for read from file
-move $a0, $s6      # file descriptor 
-la   $a1, buffer   # address of buffer to which to read
-li   $a2, 1024     # hardcoded buffer length
-syscall            # read from file
+	li   $v0, 14       # system call for read from file
+	move $a0, $s6      # file descriptor 
+	la   $a1, buffer   # address of buffer to which to read
+	li   $a2, 1024     # hardcoded buffer length
+	syscall            # read from file
 
+Store: # will store teh contents of the file in $t0
+	la $t0, buffer 
+	li  $v0, 4           # service 4 is print string
+   	add $a0, $t0, $zero  # load desired value into argument register $a0, using pseudo-op
+    	syscall
+    	
+   	#THIS IS IS LB LOOP SETUP
+    	lb $t1 , 0($t0)
+	li  $v0, 11           # service 4 is print string
+	add $a0, $t1, $zero  # load desired value into argument register $a0, using pseudo-op
+	syscall
+	
+	lb $t1 , 1($t0)
+	li  $v0, 11           # service 4 is print string
+	add $a0, $t1, $zero  # load desired value into argument register $a0, using pseudo-op
+	syscall
+	
+	lb $t1 , 2($t0)
+	li  $v0, 11           # service 4 is print string
+	add $a0, $t1, $zero  # load desired value into argument register $a0, using pseudo-op
+	syscall
+	
+	
+# Close the file
 
-la $a0, buffer        # address of string to be printed
-li $v0, 4            # print string
-syscall
-
-
-# Close the file 
 li   $v0, 16       # system call for close file
 move $a0, $s6      # file descriptor to close
 syscall            # close file
+		
+
+	
