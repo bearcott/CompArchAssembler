a57754aa61dce66f064638ff61d8e308e0790486
bearcott <bear@bearcott.com>
2016-04-24 09:38:16 -0500

    fix: consistent spacing, comments

--- FileIO/fileio.asm
+++ FileIO/fileio.asm
@@ -1,49 +1,49 @@
 .data
-fileName: .asciiz "test.asm" # filename for input
-buffer: .space 1024
-myString: .word 268501009
+	fileName: .asciiz "test.asm" # filename for input
+	buffer: .space 1024
+	myString: .word 268501009
 
-register: .space 4
-string1:  .space 20 # store the instruction "before the space"
+	register: .space 4
+	string1:  .space 20 # store the instruction "before the space"
 
 .text
 OpenFile: #open a file for writing
-	li   $v0, 13       # system call for open file
-	la   $a0, fileName # load file name
-	li   $a1, 0        # Open for reading (0 is read, 1 is write)
-	li   $a2, 0        # ignore mode ???
-	syscall            # open a file (file descriptor returned in $v0)
-	move $s6, $v0      # save the file descriptor to $s6
+	li $v0, 13 # system call for open file
+	la $a0, fileName # load file name
+	li $a1, 0 # Open for reading (0 is read, 1 is write)
+	li $a2, 0 # ignore mode ???
+	syscall # open a file (file descriptor returned in $v0)
+	move $s6, $v0 # save the file descriptor to $s6
 
 ReadFile: #read from file
-	li   $v0, 14       # system call for read from file
-	move $a0, $s6      # use file descriptor
-	la   $a1, buffer   # address of buffer to which to write to
-	la   $a2, buffer   # hardcoded buffer length
-	syscall            # read from file
+	li $v0, 14 # system call for read from file
+	move $a0, $s6 # use file descriptor
+	la $a1, buffer # address of buffer to which to write to
+	la $a2, buffer # hardcoded buffer length
+	syscall # read from file
 
 Store: # will store the contents of the file in $t0
-	la $t0, buffer       # load buffer to $t0
-	li  $v0, 4           # service 4 is print string
+	la $t0, buffer # load buffer to $t0
+	li $v0, 4 # service 4 is print string
 	add $a0, $t0, $zero  # load desired value into argument register $a0, using pseudo-op
 	syscall
 
 LbLoop:
-	li $t2, 70 # t0 is a constant THAT WE CAN CHANGE FOR THE AMOUNT OF CHARAACTERS
-	li $t3, 0 # t1 is our counter (i)
+	li $t2, 70 # t2 is a constant THAT WE CAN CHANGE FOR THE AMOUNT OF CHARAACTERS
+	li $t3, 0 # t3 is our counter (i)
 	li $s0, ' ' # load $s0 with "space"
 	li $s1, ',' # load $s1 with ","
 	li $s2,  '\n' # load $s2 with newline character
-loop:
 
+loop:
 	la $t0, buffer
 	beq $t3, $t2, end # if t3 == 70 we are done
-	addu $t0, $t0, $t3
-		lb $t1 , ($t0)
-			beq $t1, $s0, end # stop read if meet a "space"
-	li  $v0, 11           # service 4 is print string
+	addu $t0, $t0, $t3 # increment buffer by counter
+	lb $t1, ($t0) # load first byte of buffer + counter
+	beq $t1, $s0, end # stop read if meet a "space"
+	li  $v0, 11 # service 11 is print char
 	add $a0, $t1, $zero  # load desired value into argument register $a0, using pseudo-op
-	syscall
+	syscall #print current character
 	addi $t3, $t3, 1 # add 1 to t3 the counter
 	j loop # jump back to the top
 
@@ -61,7 +61,7 @@ space: # THIS IS WHERE PARSING IS DONE FOR INSTRUCTIONS
 	sb $t5, 2($s3)
 	sb $t4, 3($s3)
 
-	li  $v0, 4           # service 1 is print integer
+	li  $v0, 4     # service 1 is print integer
     	add $a0, $s3, $zero  # load desired value into argument register $a0, using pseudo-op
     	syscall
 
@@ -70,6 +70,6 @@ space: # THIS IS WHERE PARSING IS DONE FOR INSTRUCTIONS
 
 # Close the file
 end:
-	li   $v0, 16       # system call for close file
-	move $a0, $s6      # file descriptor to close
-	syscall            # close file
+	li $v0, 16 # system call for close file
+	move $a0, $s6 # file descriptor to close
+	syscall # close file
